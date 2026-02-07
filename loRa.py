8d7cfd5d25954e3a1d0c4c785e748a885061b138
Carlo <carlo.stasi10@gmail.com>
2026-02-07 16:59:07 +0100

    Attempt to increase the accuracy modifying the tokenizer.

--- loRa.py
+++ loRa.py
@@ -1,11 +1,6 @@
 import torch
-from transformers import (
-    AutoTokenizer, 
-    AutoModelForCausalLM, 
-    TrainingArguments, 
-    Trainer
-)
-from peft import LoraConfig, get_peft_model, TaskType
+from transformers import (AutoTokenizer,  AutoModelForCausalLM, TrainingArguments, Trainer)
+from peft import LoraConfig, get_peft_model, TaskType, PeftModel
 from tqdm import tqdm
 import time
 from datapreparation import load_and_prepare_data
@@ -13,50 +8,54 @@ import datapreparation
 from datasets import concatenate_datasets
 
 
-# 1. Load Data using the provided function [cite: 47]
+# 1. Load Data using the provided function 
 train_ds, val_ds, test_ds, label_list, label2id, id2label = datapreparation.load_and_prepare_data()
 print("Original Training size:", len(train_ds))
+#balanced dataset
+def get_balanced_dataset(input_ds, num_samples=3000):
+    tech_support = input_ds.filter(lambda x: x['queue'] == 'Technical Support')
+    others = input_ds.filter(lambda x: x['queue'] != 'Technical Support')
+    tech_support = tech_support.shuffle(seed=42).select(range(num_samples))
+    balanced_train_ds = concatenate_datasets([tech_support, others])
+    balanced_train_ds = balanced_train_ds.shuffle(seed=42)
+    return balanced_train_ds
 
+# Call the function correctly
+train_ds = get_balanced_dataset(train_ds, num_samples=3000)
 # Check device
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {device}")
-model_name = "gpt2" 
+model_name = "distilgpt2" 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-#by default is not defined
-tokenizer.pad_token = tokenizer.eos_token
 model = AutoModelForCausalLM.from_pretrained(model_name)
+tokenizer.add_special_tokens({"pad_token": "[PAD]"})
+# Carica modello base
+base_model = AutoModelForCausalLM.from_pretrained(model_name)
+# Carica i tuoi adattatori LoRA sopra il modello base
+
+model.resize_token_embeddings(len(tokenizer))
+
+tokenizer.padding_side = "right"
+
 
 peft_config = LoraConfig(
     task_type=TaskType.CAUSAL_LM,
     inference_mode=False,
-    r=32,
-    lora_alpha=64,
+    r=16,
+    lora_alpha=32,
     lora_dropout=0.1,
-    target_modules=["c_attn"]
+    target_modules=["c_attn","c_proj", "c_fc"]
 )
 model = get_peft_model(model,peft_config)
 
 def prompt_format(subject,body,label=None):
     clean_body = body[:800]
-    prompt = f"""Classify customer emails into exaclty one of the following departments.
-
-Examples:
-Email: Password reset not working
-Department: Technical Support
-
-Email: I want a refund for order #123
-Department: Billing and Payments
-
-Email: Tell me about your enterprise plan
-Department: Sales and Pre-Sales
-
-Email: How do I change my settings?
-Department: Customer Service
-
-Email: General question about your company
-Department: General Inquiry
-
-Now classify:
+    prompt = f"""Classify each email into exactly ONE of these departments:
+- Technical Support
+- Customer Service
+- Billing and Payments
+- Sales and Pre-Sales
+- General Inquiry
 Email: Subject: {subject}
 {clean_body}
 Department:"""
@@ -70,7 +69,15 @@ def preprocess_function(examples):
     for subj, body, queue in zip(examples['subject'], examples['body'], examples['queue']):
         prompts.append(prompt_format(subj,body,queue))
     model_input = tokenizer(prompts,max_length=512,truncation=True,padding="max_length")
-    model_input["labels"] = model_input["input_ids"].copy()
+    input_ids = model_input["input_ids"]
+    labels = []
+    for seq in input_ids:
+        seq_label = [token for token in seq]
+        for i, token in enumerate(seq_label):
+            if token == tokenizer.pad_token_id: 
+                seq_label[i] = -100
+        labels.append(seq_label)
+    model_input["labels"] = labels
     return model_input
 print(f"preprocessing datasets ")
 tokenized_train = train_ds.map(preprocess_function, batched=True, remove_columns=train_ds.column_names)
@@ -80,13 +87,13 @@ print(f"validation size : {len(tokenized_val)}")
 
 training_args = TrainingArguments(
     output_dir="./lora_email_classifier",
-    num_train_epochs=3,              
+    num_train_epochs=5,              
     per_device_train_batch_size=4,   
     per_device_eval_batch_size=8,
     gradient_accumulation_steps=2,   
     learning_rate=2e-4,              
     weight_decay=0.01,
-    logging_steps=50,
+    logging_steps=50, 
     eval_strategy="epoch",
     save_strategy="epoch",
     load_best_model_at_end=True,
@@ -116,14 +123,14 @@ def department_prediction(subject,body):
     inputs = tokenizer(prompt, return_tensors="pt", truncation=True, max_length=512).to(device)
     outputs = model.generate(**inputs,max_new_tokens=15, pad_token_id=tokenizer.eos_token_id,do_sample=False)
     generated = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    marker = "Best matching department:"
+    marker = "Department:"
     if marker.lower() in generated.lower():
         answer = generated.lower().split(marker.lower())[-1].strip()
     else:
         answer = ""
     return answer
 
-def departments_match(prediction, valid_labels):
+def departments_matching(prediction, valid_labels):
     pred_lower = prediction.lower().strip()
     for label in valid_labels:
         if pred_lower == label.lower():
@@ -147,7 +154,7 @@ true_labels = []
 for i in tqdm(range(total), desc="Testing"):
     sample = test_ds[i]
     pred_text = department_prediction(sample['subject'], sample['body'])
-    pred_label = departments_match(pred_text, label_list)
+    pred_label = departments_matching(pred_text, label_list)
     
     if pred_text and pred_label == sample['queue']:
         correct += 1
@@ -158,9 +165,8 @@ for i in tqdm(range(total), desc="Testing"):
 accuracy = (correct / total) * 100
 
 print("\n" + "="*60)
-print(f"MODEL: GPT2 + LoRA")
+print(f"MODEL: GPT2 + LoRA ")
 print("="*60)
 print(f"Accuracy: {accuracy:.2f}%")
 print(f"Training time: {train_time:.2f}s ({train_time/60:.1f} min)")
 print("="*60)
-
